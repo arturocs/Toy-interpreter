fe42dcf09fa7b089fccd9f5e72202c4e79bf586e
Arturo <arturocs@protonmail.com>
2020-08-27 19:54:15 +0200

    Try to use regex

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
+use regex::Regex;
 use std::collections::HashMap;
 use std::env;
 use std::fs;
-
 enum VarType<'a> {
     Number(f64),
     Bool(bool),
@@ -35,49 +35,93 @@ enum Token<'a> {
     VarName(&'a str),
     True,
     False,
+    Comma,
+    Number,
+    String(&'a str),
+    NewLine,
 }
 
 fn process_unknown(unknown: &str) -> Vec<Token> {
-    unimplemented!("string desconocido: {}",unknown)
+    unimplemented!("string desconocido: {}", unknown)
+}
+fn tokenizer(source_code: &str) -> Vec<Token> {
+    let re = Regex::new(
+        r#"var\s|if\s|else\s|while\s|==|!=|!|=|<|>|\+|-|\*|\{|\}|[|]|\(|\)|\strue\s|\sfalse\s|,|\d+.?\d*|"(\w+\s*)*"|\n|\w+"#,
+    )
+    .unwrap();
+    re.captures_iter(source_code).inspect(|c|println!("{:?}",c)).map(|cap|cap.get(0).unwrap().into())
+        .map(|cap : &str| match cap.trim() {
+            "var" => Token::VarDeclaration,
+            "if" => Token::If,
+            "else" => Token::Else,
+            "while" => Token::While,
+            "=" => Token::OpAssignation,
+            "==" => Token::OpEqual,
+            "!=" => Token::OpNotEqual,
+            "!" => Token::OpNot,
+            "<" => Token::OpLessThan,
+            ">" => Token::OpGreaterThan,
+            "+" => Token::OpAdd,
+            "-" => Token::OpNeg,
+            "*" => Token::OpMul,
+            "{" => Token::OpenCBrackets,
+            "}" => Token::CloseCBrackets,
+            "[" => Token::OpenBrackets,
+            "]" => Token::CloseBrackets,
+            "(" => Token::OpenParentheses,
+            ")" => Token::CloseParentheses,
+            "true" => Token::True,
+            "false" => Token::False,
+            unknown => Token::Unknown(unknown),
+        })
+        .flat_map(|token| match token {
+            Token::Unknown(unknown_sequence) => process_unknown(unknown_sequence).into_iter(),
+            other => vec![other].into_iter(),
+        })
+        .collect()
 }
 
-fn parse(source_code: &str) -> Vec<Token>{
-    source_code.split_whitespace().map(|word| match word {
-        "var" => Token::VarDeclaration,
-        "if" => Token::If,
-        "else" => Token::Else,
-        "while" => Token::While,
-        "=" => Token::OpAssignation,
-        "==" => Token::OpEqual,
-        "!=" => Token::OpNotEqual,
-        "!" => Token::OpNot,
-        "<" => Token::OpLessThan,
-        ">" => Token::OpGreaterThan,
-        "+" => Token::OpAdd,
-        "-" => Token::OpNeg,
-        "*" => Token::OpMul,
-        "{" => Token::OpenCBrackets,
-        "}" => Token::CloseCBrackets,
-        "[" => Token::OpenBrackets,
-        "]" => Token::CloseBrackets,
-        "(" => Token::OpenParentheses,
-        ")" => Token::CloseParentheses,
-        "true" => Token::True,
-        "false" => Token::False,
-        unknown => Token::Unknown(unknown),
-    }).flat_map(|token| match token {
-        Token::Unknown(unknown_sequence) => process_unknown(unknown_sequence).into_iter(),
-        other => vec![other].into_iter(),
-    }).collect()
+fn parse(source_code: &str) -> Vec<Token> {
+    source_code
+        .split_whitespace()
+        .map(|word| match word {
+            "var" => Token::VarDeclaration,
+            "if" => Token::If,
+            "else" => Token::Else,
+            "while" => Token::While,
+            "=" => Token::OpAssignation,
+            "==" => Token::OpEqual,
+            "!=" => Token::OpNotEqual,
+            "!" => Token::OpNot,
+            "<" => Token::OpLessThan,
+            ">" => Token::OpGreaterThan,
+            "+" => Token::OpAdd,
+            "-" => Token::OpNeg,
+            "*" => Token::OpMul,
+            "{" => Token::OpenCBrackets,
+            "}" => Token::CloseCBrackets,
+            "[" => Token::OpenBrackets,
+            "]" => Token::CloseBrackets,
+            "(" => Token::OpenParentheses,
+            ")" => Token::CloseParentheses,
+            "true" => Token::True,
+            "false" => Token::False,
+            unknown => Token::Unknown(unknown),
+        })
+        .flat_map(|token| match token {
+            Token::Unknown(unknown_sequence) => process_unknown(unknown_sequence).into_iter(),
+            other => vec![other].into_iter(),
+        })
+        .collect()
 }
 
-fn execute(instructions:Vec<Token>){
+fn execute(instructions: Vec<Token>) {
     let variables: HashMap<&str, VarType> = HashMap::new();
 }
 
 fn main() {
     let filename = env::args().nth(1).expect("falta argumento");
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
-    let instructions = parse(&contents);
+    let instructions = tokenizer(&contents);
     execute(instructions);
 }
