40650a96a59d2da0bc876ae2036c55f333f6ada3
Arturo <arturocs@protonmail.com>
2020-09-25 00:35:25 +0200

    Removed tests from main

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,21 @@
 #![allow(dead_code)]
 use std::{env, fs};
 use v_eval::Eval;
+mod expr_eval;
 mod parser;
 mod runtime;
 mod tokenizer;
-mod val;
 #[macro_use]
 extern crate lazy_static;
 use crate::parser::*;
 use crate::tokenizer::*;
-
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let t = tokenize_expression("(4*5*(6*7))")?;
-    dbg!(&t);
-    let a = parse_expr(&t)?;
-    dbg!(&a);
     let env = Eval::default();
     let filename = env::args().nth(1).ok_or("Missing argument")?;
     let contents = fs::read_to_string(filename)?;
     let instructions = tokenize(&contents);
-    //let instructions = dbg!(instructions);
+    let instructions = dbg!(instructions);
     let ast = parse(&instructions)?;
-    //dbg!(&ast);
-    //runtime::execute(&ast, env)?;
+    runtime::execute(&ast, env)?;
     Ok(())
 }
